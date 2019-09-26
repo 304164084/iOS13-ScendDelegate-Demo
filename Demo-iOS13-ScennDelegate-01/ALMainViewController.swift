660ad7ef487a74449b82d9b443e221e9a8823e1f
suibanglin <suibanglin@kingsoft.com>
2019-09-26 15:07:11 +0800

    upload the first

---
+++ Demo-iOS13-ScennDelegate-01/ALMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ALMainViewController.swift
+//  Demo-iOS13-ScennDelegate-01
+//
+//  Created by Amazing Life on 2019/9/26.
+//  Copyright © 2019 Amazing Life. All rights reserved.
+//
+
+import UIKit
+
+class ALMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
